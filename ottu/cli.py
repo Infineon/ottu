4e791fc3caf863ce88197542613a7b482187bf16
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2026-08-17 14:44:39 +0200

    ottu: Add minimal cli entry point.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

---
+++ ottu/cli.py
@@ -0,0 +1,19 @@
+"""Main CLI entry point for OTTU."""
+
+import click
+
+from ottu import __version__
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    """ottu - On-target testing utility for embedded boards."""
+    pass
+
+def main():
+    """Entry point for the CLI."""
+    cli()
+
+
+if __name__ == "__main__":
+    main()
