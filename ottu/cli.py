850ff5da18873eb4367764b238181c86a72a3fff
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2026-08-18 13:39:37 +0200

    ottu/cli: Add return type annotation.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- ottu/cli.py
+++ ottu/cli.py
@@ -7,12 +7,12 @@ from ottu import __version__
 
 @click.group()
 @click.version_option(version=__version__)
-def cli():
+def cli() -> None:
     """ottu - On-target testing utility for embedded boards."""
     pass
 
 
-def main():
+def main() -> None:
     """Entry point for the CLI."""
     cli()
 
