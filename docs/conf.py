1ce0c085bfd175386d0f6249c6587e404a95b99c
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2026-08-21 13:48:58 +0200

    docs: Add minimal read the docs file enablement.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = "ottu"
+author = "Infineon Technologies AG"
+copyright = "- The ottu Documentation is Copyright © 2026, Infineon Technologies AG"
+
+
+source_suffix = ".rst"
+highlight_language = "python"
+primary_domain = "py"
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinx_rtd_theme"
+master_doc = "index"
